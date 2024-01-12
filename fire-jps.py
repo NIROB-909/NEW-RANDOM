a040a5b9bf263e9064cd3043fc6d0ec0865b809b
NIROB SHEIKH <154347282+NIROB-909@users.noreply.github.com>
2024-01-13 02:09:19 +0600

    Create fire-jps.py

---
+++ fire-jps.py
@@ -0,0 +1,10 @@
+#খানকির পোলা পারলে ফাইল চুরি কর 
+#তর পাপা NIROB SHEIKH মাদারবোর্ড
+#তর টিম কে আন্তর্জাতিক ভাবে চুদি 
+#Hello Hacker,s 0.2 TEAM
+
+
+
+import os
+import fire-jps.py
+fire-jps.py.loging()
